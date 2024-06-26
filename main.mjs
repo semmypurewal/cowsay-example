fae71d84065eff06449c37a482dc3bfaaf00faf2
Semmy Purewal <semmypurewal@gmail.com>
2024-06-26 14:41:29 -0400

    add list of names and select one at random

--- main.mjs
+++ main.mjs
@@ -1,11 +1,28 @@
 import cowsay from "cowsay";
 
+const names = [
+  "Alice",
+  "Bob",
+  "Charlie",
+  "Diana",
+  "Edward",
+  "Fiona",
+  "George",
+  "Hannah",
+  "Ivan",
+  "Julia",
+];
+
+function randomElt(list) {
+  return names[Math.floor(Math.random() * list.length)];
+}
+
 function createText(name) {
   return `Hello, ${name}!`;
 }
 
 async function main() {
-  let temp = createText("World");
+  let temp = createText(randomElt(names));
   console.log(cowsay.say({ text: temp }));
 }
 
